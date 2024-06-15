abca000c613780cca5fdc767531b769ae49c332a
myhe01 <cpbro123@gmail.com>
2024-06-15 19:11:27 -0400

    Draw string modified size and padding

--- lib/include/bgfx.h
+++ lib/include/bgfx.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 #include "bgfx_font.h"
+#include "bgfx_letters.h"
 
 /* Data Types */
 typedef struct bgfx_properties
@@ -49,36 +50,6 @@ int bgfx_draw_line_x(int start_x, int finish_x, int y);
 // Returns 0 on success, -1 on failure.
 int bgfx_draw_line_y(int x, int start_y, int finish_y);
 
-// Draw a letter with the top left starting at x,y
-// char letter: character to draw
-// int x: x-coordinate of user map.
-// int y: y-coordinate of user map.
-// Returns 0 on success, -1 on failure.
-int bgfx_draw_letter(char letter, int x, int y);
-
-// Draw a letter of larger size with the top left starting at x,y
-// char letter: character to draw
-// int size_modifier: Size modifier for the character
-// int x: x-coordinate of user map.
-// int y: y-coordinate of user map.
-// Returns 0 on success, -1 on failure.
-int bgfx_draw_letter_modified(char letter, int size_modifier, int x, int y);
-
-// Draw a string horizontally with the top left starting at x,y.
-// char * string: string to draw
-// int x: x-coordinate of user map.
-// int y: y-coordinate of user map.
-// Returns 0 on success, -1 on failure.
-int bgfx_draw_string(char * string, int x, int y);
-
-// Draw a string horizontally with padding, with the top left starting at x,y.
-// char * string: string to draw
-// int pad: number of pixels to pad between each letter
-// int x: x-coordinate of user map.
-// int y: y-coordinate of user map.
-// Returns 0 on success, -1 on failure.
-int bgfx_draw_string_padding(char * string, int pad, int x, int y);
-
 /* Private Functions */
 // Perform error checking on a set of coordinates to see if they're valid
 // int x: x-coordinate of user map.
@@ -90,18 +61,5 @@ bool _bgfx_valid(int x, int y);
 // int x: x-coordinate of user map.
 // int y: y-coordinate of user map.
 void _bgfx_add_point(int x, int y);
-
-// Check if there is a pixel present at (x,y) for a specific letter's map
-// int x: x-coordinate of letter map
-// int y: y-coordinate of letter map
-// Returns true if pixel present, false if not
-bool _bgfx_letter_point(uint8_t const * letter, int x, int y);
-
-// Draw a letter with the top left starting at x,y
-// char letter: character to draw
-// int size_modifier: Size modifier for the character
-// int x: x-coordinate of user map.
-// int y: y-coordinate of user map.
-void _bgfx_draw_letter(char letter, int size_modifier, int x, int y);
 
 #endif // BGFX_H
